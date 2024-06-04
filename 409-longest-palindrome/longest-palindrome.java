9babd2ef2069badc72490e027d68d861530ac13e
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-06-04 22:50:06 +0530

    Time: 8 ms (32.41%) | Memory: 41.7 MB (46.48%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character,Integer> charct = new HashMap<>();
+        for(char c : s.toCharArray()){
+            if(charct.containsKey(c)){
+                charct.put(c,charct.get(c)+1);
+            }
+            else{
+                charct.put(c,1);
+            }
+        }
+        int count = 0;
+        boolean hasOdd = false;
+        for(Map.Entry<Character,Integer> entry : charct.entrySet()){
+            int val = entry.getValue();
+            if(val%2 == 1){
+                hasOdd = true;
+            }
+            count += val/2*2;
+        }
+        return hasOdd ? count+1 : count;
+    }
+}
