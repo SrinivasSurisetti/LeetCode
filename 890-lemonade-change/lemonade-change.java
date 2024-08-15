256bcf956ff654577977364f8bb14c9bcefd3c30
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-08-15 22:41:06 +0530

    Time: 2 ms (95.27%) | Memory: 55.3 MB (97.55%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean lemonadeChange(int[] bills) {
         int numOf5= 0;
         int numOf10 = 0;
-        int numOf20 = 0;
         for(int ele : bills){
             if(ele==5){
                 numOf5++;
@@ -12,7 +11,7 @@ class Solution {
                 numOf5--;
             }
             else if(ele==20 && ((numOf5>=1 &&numOf10>=1) || (numOf5>=3))){
-                if(numOf5>=1 &&numOf10>=1){
+                if(numOf5>=1 && numOf10>=1){
                     numOf10--;
                     numOf5--;
                 }
